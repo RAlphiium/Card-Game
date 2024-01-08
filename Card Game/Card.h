b76684a9dac1ff912ae66b88261efba71344756c
RAlphiium <153527922+RAlphiium@users.noreply.github.com>
2024-01-08 16:11:10 +0000

    Created a Player class

--- Card Game/Card.h
+++ Card Game/Card.h
@@ -7,6 +7,7 @@ private:
 	string suit;
 	string symbol;
 	int value;
+	//May add an orientation attribute later (used to tell if card is faced up/down or rotated)
 public:
 	Card(string suit,string symbol,int value);
 
